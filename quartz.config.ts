30c37b4a5c7563964ecacc8d0507159857bec822
Alex <24763887+QWxleA@users.noreply.github.com>
2025-05-12 21:15:56 +0200

    initial setup, testing locally

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "Bold studies",
+    pageTitleSuffix: "notes and half-truths",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
