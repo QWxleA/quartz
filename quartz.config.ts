843a1c0244238a30cf5a733f3b36427133896fef
Alex <24763887+QWxleA@users.noreply.github.com>
2025-05-12 21:33:03 +0200

    initial setup, less annoying

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "qwxlea.org",
     ignorePatterns: [
       "Private", 
       "Templates", 
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "qwxlea@GitHub": "https://github.com/qwxlea",
+      // "Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
 }
