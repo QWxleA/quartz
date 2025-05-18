5079e69d2c32d049ad3a4c7ebd6f3db7d463bcf5
Alex <24763887+QWxleA@users.noreply.github.com>
2025-05-18 18:08:43 +0200

    Quartz sync: May 18, 2025, 6:08 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -93,7 +93,7 @@ const config: QuartzConfig = {
     ],
     filters: [
       Plugin.RemoveDrafts(),
-      Plugin.ExplicitPublish(), 
+      // Plugin.ExplicitPublish(), 
     ],
     emitters: [
       Plugin.AliasRedirects(),
