3dfec1b63ee1f33620481246f6f366e4fd82aea6
Alex <24763887+QWxleA@users.noreply.github.com>
2025-05-21 21:45:42 +0200

    Revert "added logo v1"
    This reverts commit af27290ec7e688bb246305b1598da4d129612136.

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Bold studies",
+    pageTitle: "Bolding studies",
     pageTitleSuffix: "notes and 🌱 half-truths",
     enableSPA: true,
     enablePopovers: true,
--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -1,28 +1,14 @@
-import { joinSegments, pathToRoot } from "../util/path"
+import { pathToRoot } from "../util/path"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
 import { i18n } from "../i18n"
 
-// const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
-//   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
-//   const baseDir = pathToRoot(fileData.slug!)
-//   return (
-//     <h2 class={classNames(displayClass, "page-title")}>
-//       <a href={baseDir}>{title}</a>
-//     </h2>
-//   )
-// }
-
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
-  const iconPath = joinSegments(baseDir, "static/icon.png")
   return (
     <h2 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>
-        <img alt={title} />
-        <div>{title}</div>
-      </a>      
+      <a href={baseDir}>{title}</a>
     </h2>
   )
 }
